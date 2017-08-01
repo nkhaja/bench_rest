5e8eb2a80a0be582ac9815fd0721b54f5134096c
nkhaja <nbl.khaja@gmail.com>
2017-07-31 23:51:55 -0700

    get_page function available for selecting page of url

--- benchRest.py
+++ benchRest.py
@@ -9,7 +9,21 @@ totalCount = "totalCount"
 page = 'page'
 transactions = 'transactions'
 
+base_url = 'http://resttest.bench.co/transactions'
+url_cap = '.json'
+
+
+
+def getPage(page_num):
+
+    requested_url = base_url + str(page_num) + url_cap
+    response = urllib2.urlopen(requested_url)
+
+    return response
+
+
+
 if __name__ == '__main__':
 
-    foo = urllib2.urlopen("http://resttest.bench.co/transactions/4.json").read()
+    foo = urllib2.urlopen('http://resttest.bench.co/transactions/4.json').read()
     print foo
